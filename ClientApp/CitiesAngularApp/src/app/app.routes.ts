a9f1aaa85d6105f53200d14982b2ca932a1ddee5
joseluiscontreras <joseluis.contreras@innocv.com>
2024-01-31 12:27:42 +0100

    added cities component

--- ClientApp/CitiesAngularApp/src/app/app.routes.ts
+++ ClientApp/CitiesAngularApp/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { CitiesComponent } from './cities/cities.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: "cities", component: CitiesComponent }];
